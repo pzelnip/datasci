d159365c8cf3c1709f907cc99281f571e9611db2
Adam Parkin <aparkin@seekersolutions.com>
2013-05-06 11:42:36 -0700

    Added solution to problem 2 of assignment 1

--- src/first_assn/tweet_sentiment.py
+++ src/first_assn/tweet_sentiment.py
@@ -1,17 +1,92 @@
+'''
+Assignment 1, Problem 2 -- derive the sentiment of each tweet
+
+Created on May 6, 2013
+
+@author: aparkin
+'''
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def load_base_sentiment_data(sentiment_file):
+    '''
+    Given a file containing sentiment data in the form:
+    
+    word\tscore
+    
+    returns a dictionary mapping words to their sentiment score
+    '''
+    sentiment_values = {}
+    with open(sentiment_file) as fobj:
+        for line in fobj:
+            tokens = line.strip().split('\t')
+            sentiment_values[tokens[0]] = int(tokens[1])
+            
+    return sentiment_values
+
+
+def read_tweets_from_file(tweet_file):
+    '''
+    Given a file containing raw JSON as returned by the Twitter API, returns
+    a list of extracted tweets
+    '''
+    with open(tweet_file) as fobj:
+        data = json.load(fobj)
+        tweets = [tweet['text'].encode('utf-8') for tweet in data['results']]
+    return tweets
+
+
+def get_sentiments_for_tweets(tweets, sentiments):
+    '''
+    Given a list of tweets, and a collection of sentiment scores, return a list
+    of tuples mapping each tweet to its total sentiment score
+    '''
+    result = []
+    for tweet in tweets:
+        words = tweet.split()
+        total = 0
+        for word in words:
+            total += sentiments.get(word, 0)
+        result.append((tweet, total))
+    return result
+
+
+def dump_results(tweet_sentiments):
+    '''
+    Print the results of a call to get_sentiments_for_tweets() to STDOUT
+    '''
+    for (tweet, sentiment) in tweet_sentiments:
+        print("<%s:%s>" % (tweet, sentiment))
+
+
+def process_args():
+    '''
+    Process the command line arguments into a dictionary, or calls show_help()
+    if arguments are malformed
+    '''
+    if len(sys.argv) < 3:
+        show_help()
+        
+    result = {}
+    result['sentiment_file'] = sys.argv[1]
+    result['tweet_file'] = sys.argv[2]
+    return result
+
+
+def show_help():
+    ''' Display help screen & exit '''
+    print("Usage is %s <sentiment_file> <tweet_file>" % sys.argv[0])
+    sys.exit()
+
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    args = process_args()
+    sentiments = load_base_sentiment_data(args['sentiment_file'])
+    tweets = read_tweets_from_file(args['tweet_file'])
+    tweet_sentiments = get_sentiments_for_tweets(tweets, sentiments)
+    dump_results(tweet_sentiments)
 
+    
 if __name__ == '__main__':
     main()
