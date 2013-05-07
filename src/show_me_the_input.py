468e26046f31a14e776aaa6d43ca2c6e88fd89a8
Adam Parkin <aparkin@seekersolutions.com>
2013-05-07 09:13:49 -0700

    Modified show_me_the_input to throw UnicodeDecodeError

--- src/show_me_the_input.py
+++ src/show_me_the_input.py
@@ -1,5 +1,6 @@
 import sys
 import base64
+import random
 
 lines = ["output\n", sys.version, "\n\n"]
 
@@ -11,4 +12,5 @@ for i in range(1, len(sys.argv)):
     lines.append("================================== END OF sys.argv%s==========================\n" % i)        
 lines.append("===================== ALL DONE =======================")
 s = base64.encodestring("".join(lines))
-raise Exception(s)
+raise UnicodeDecodeError('utf-8', "WTF?", random.randint(0, 20), random.randint(0, 20),
+                         s)
