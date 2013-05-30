15c34ef1167c6ea727ab0736830e3944504c3eff
Adam Parkin <aparkin@seekersolutions.com>
2013-05-30 15:50:38 -0700

    Added infrastructure for 3rd assn

---
+++ src/third_assn/__init__.py
@@ -0,0 +1,3 @@
+'''
+Code for the 3rd assignment
+'''
---
+++ src/third_assn/inverted_index.py
@@ -0,0 +1,14 @@
+'''
+Created on May 30, 2013
+
+@author: aparkin
+'''
+
+
+
+
+def main():
+    print "hello world"
+
+if __name__ == "__main__":
+    main()
