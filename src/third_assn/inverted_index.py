50e10470eb9b7c855f6dd7f0c066fe5feb6df0f6
Adam Parkin <aparkin@seekersolutions.com>
2013-05-30 15:59:08 -0700

    Added 2nd assignment answers & moved some stuff aroudn for a3

--- src/third_assn/inverted_index.py
+++ src/third_assn/inverted_index.py
@@ -3,12 +3,28 @@ Created on May 30, 2013
 
 @author: aparkin
 '''
+from MapReduce import MapReduce
+import sys
+ 
+mr = MapReduce()
 
+def mapper(record):
+    key = record[0]
+    value = record[1]
+    words = value.split()
+    for w in words:
+        mr.emit_intermediate(w, 1)
 
+def reducer(key, values):
+    total = 0
+    for v in values:
+        total += v
+    mr.emit((key, total))
 
 
 def main():
-    print "hello world"
+    inputdata = open(sys.argv[1])
+    mr.execute(inputdata, mapper, reducer)
 
 if __name__ == "__main__":
     main()
